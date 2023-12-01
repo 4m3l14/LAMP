cdf89905be08bcb4eb3e1029b41a570ecfb97034
4m3l14 <amelia.wannaku3@gmail.com>
2023-12-01 08:46:25 +0000

    update

---
+++ mysql/db_videoteca.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE IF NOT EXISTS videoteca;
+USE videoteca;
+
+create TABLE attore
+(
+    ID_attore int not null AUTO_INCREMENT PRIMARY KEY,
+    nome varchar(64) not null,
+    cognome varchar(64) not null,
+    nascita date
+);
+
+CREATE TABLE film(
+    ID_film int not null AUTO_INCREMENT PRIMARY KEY,
+    nome varchar(64) not null,
+    genere varchar(64)not null
+);
+
+CREATE TABLE contratto(
+ID_attore int NOT null ,
+    ID_film int NOT null,
+    FOREIGN KEY (ID_attore)REFERENCES attore(ID_attore),
+    FOREIGN KEY (ID_film)REFERENCES film(ID_film)
+);
+
