3751943d20a86021ae79e0bf10032050804cf49b
4m3l14 <amelia.wannaku3@gmail.com>
2024-03-01 09:02:46 +0000

    update

--- mysql/campionato.sql
+++ mysql/campionato.sql
@@ -2,26 +2,32 @@
 CREATE DATABASE IF NOT EXISTS Campionato;
 USE Campionato;
 
--- Creazione della tabella "Calciatori"
-create TABLE calciatori(
-    cognome varchar(64) not null,
-    ruolo int (64 )not null,
-    stipendio int (64)
-    ID_calciatore int not null AUTO_INCREMENT PRIMARY KEY,
-    ID_capitano int (64),
-    nascita date,
-    FOREIGN KEY (FK_calciatore)REFERENCES squadre (ID_squadre)
-);
-
 -- Creazione della tabella "Squadre"
 create TABLE squadre(
     nome_squadra varchar(64) not null,
     ID_squadra int not null AUTO_INCREMENT PRIMARY KEY
 );
 
+-- Creazione della tabella "Calciatori"
+create TABLE calciatori(
+    cognome varchar(64),
+    ruolo varchar (64),
+    stipendio int,
+    ID_calciatore int not null AUTO_INCREMENT PRIMARY KEY,
+    ID_capitano int,
+    nascita date,
+    FK_squadre int,
+    FOREIGN KEY (FK_squadre) REFERENCES squadre (ID_squadra)
+);
+
+
+
 -- Creazione della tabella "Valutazioni"
 create TABLE valutazioni(
     voto int (64),
     ID_valutazione int not null AUTO_INCREMENT PRIMARY KEY,
-    FOREIGN KEY (FK_valutazione)REFERENCES calciatori (ID_calciatore)
+    FK_calciatori int,
+    FOREIGN KEY (FK_calciatori) REFERENCES calciatori (ID_calciatore)
 );
+
+SHOW TABLES;
