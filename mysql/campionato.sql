f22d9ee8d1abb4dfa2c4bd3548abf19329af702c
4m3l14 <amelia.wannaku3@gmail.com>
2024-03-05 08:07:21 +0000

    update

--- mysql/campionato.sql
+++ mysql/campionato.sql
@@ -7,6 +7,7 @@ create TABLE squadre(
     nome_squadra varchar(64) not null,
     ID_squadra int not null AUTO_INCREMENT PRIMARY KEY
 );
+SHOW TABLES;
 
 -- Creazione della tabella "Calciatori"
 create TABLE calciatori(
@@ -19,7 +20,7 @@ create TABLE calciatori(
     FK_squadre int,
     FOREIGN KEY (FK_squadre) REFERENCES squadre (ID_squadra)
 );
-
+SHOW TABLES;
 
 
 -- Creazione della tabella "Valutazioni"
@@ -29,5 +30,24 @@ create TABLE valutazioni(
     FK_calciatori int,
     FOREIGN KEY (FK_calciatori) REFERENCES calciatori (ID_calciatore)
 );
-
 SHOW TABLES;
+
+/*
+1)QUERY CHE MODIFICA IL CAMPO CALCIATORI 
+ALTER TABLE calciatori 
+CHANGE COLUMN ruolo ruolo VARCHAR(64);
+
+2)inserire istanza nella tabella calciatori
+INSERT INTO calciatori (ruolo)
+VALUES ('attaccante');
+
+3) modificare nella tabella calciatori il cognome Rossi con il cognome Bianchi
+UPDATE calciatori
+SET cognome='Bianchi'
+WHERE cognome='Rossi';
+
+4)cancellare nella tabella quadre l'istanza con nome squadra=Verdi
+DELETE FROM squadre
+WHERE nome_squadra='Verdi';
+
+*/
