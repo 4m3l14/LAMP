7fda30edbb0b0f23688fa3f3600a62a01c1f6037
4m3l14 <amelia.wannaku3@gmail.com>
2024-04-11 09:09:27 +0000

    update

--- www/ES05/index.php
+++ www/ES05/index.php
@@ -4,9 +4,8 @@
         <title>BENVENUTO</title>
     </head>
     <body>
-        <h1>es05 .....</h1>
+        <h1>LOGIN</h1>
         <h1></h1>
-        <a href="riservata.php">Vai all'area riservata del sito<a><br>
-        <a href="login.php">Login</a>
+        <a href="login.php">Login<a><br>
     </body>
 </html>
--- www/ES05/login.php
+++ www/ES05/login.php
@@ -1,15 +1,8 @@
 <?php
-session_start();
-if (isset($_SESSION['username'])) {
-    header('Location: riservata.php');
-    exit();
-}
+require 'functions.php';
 
-// Costanti per la connessione al database
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'db_user');
-define('DB_PASSWORD', 'db_pwd');
-define('DB_NAME', 'nome_database');
+session_start(); // Avvia la sessione php.
+$_SESSION['pag_count']++; echo $_SESSION['pag_count']; //TODO: debug only
 
 // Connessione al database
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
@@ -19,47 +12,30 @@ if (!$conn) {
     die("Connessione fallita: " . mysqli_connect_error());
 }
 
+echo "Connessione al database riuscita";
 
+// Chiudere la connessione quando non è più necessaria
+mysqli_close($conn);
 
-$valido_s = 'mrossi';
-$valido_p = 'Ciao123';
-$err_message = '';
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    if ($username === $valido_s && $password === $valido_p) {
-        $_SESSION['username'] = $username;
-        header('Location: riservata.php');
-        exit();
-    } else {
-        $err_message = 'Attenzione! Le credenziali inserite non sono corrette.';
-    }
-}
+$email = (isset($_POST['email'])) ? $_POST['email'] : "";
+$password = (isset($_POST['password'])) ? $_POST['password'] : "";
+echo "-".$email."-".$password."-"; //TODO: debug only
+list($retval,$errmsg)=login($email, $password);
+if($retval) {header("location: welcome.php"); die();} 
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-    <title>LOGIN</title>
-</head>
+<head><title>Login</title></head>
 <body>
-    <h1>Vai all'area riservata del sito.</h1>
-    <form method="POST">
-        <label for="username">Nome utente:</label>
-        <input type="text" name="username" id="username" required><br><br>
-        
-        <label for="password">Password:</label>
-        <input type="password" name="password" id="password" required><br><br>
-        <input type="submit" value="Accedi">
-    </form>
-    <?php
-
-    if (!empty($err_message)) {
-        echo "<p>$err_message</p>";
-    }
-    ?>
+  <h3>Pagina di login</h3>
+  <?=$errmsg?>
+  <form action="login.php" method="post">
+    <label>Email <input type="text" name="email" /></label><br />
+    <label>Password <input type="password" name="password"/></label><br />
+    <input type="submit" value="Login" /><input type="reset" value="Cancel" />
+  </form>
+  <p>Non hai un account? <a href="register.php">Registrati adesso</a>.</p>        
 </body>
 </html>
+
