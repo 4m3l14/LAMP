9718826b746c02f8f3d02886aca9aeb5c18e8cf5
4m3l14 <amelia.wannaku3@gmail.com>
2024-04-18 10:03:15 +0000

    update

--- www/ES05/login.php
+++ www/ES05/login.php
@@ -10,50 +10,72 @@ session_start();
 // Recupera le credenziali dalla richiesta POST
 $username = $_POST['username'] ?? "";
 $password = $_POST['password'] ?? "";
+$postreq = $_SERVER['REQUEST_METHOD'] == 'POST';
+
+//Form di login
+$html_form = <<<FORM
+<form action="$_SERVER[PHP_SELF]" method="post">
+  <label for="username"> </label><input type="text" name="username" placeholder="username" required/><br />
+  <label for="password"> </label><input type="password" name="password" placeholder="Password" required/><br />
+  <input type="submit" value="Login" /><input type="reset" value="Cancel" />
+</form>
+FORM;
+
 
 // Connessione al database
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 // Verifica della connessione
 if (!$conn) {die("Connessione fallita: " . mysqli_connect_error());}
 
-echo "Connessione al database riuscita<br>";
+//echo "Connessione al database riuscita<br>";
 
 // Esegui la query per verificare le credenziali dell'utente
 $query = "SELECT * FROM utente WHERE Username = '$username' AND Password = '$password';";
 $result = mysqli_query($conn, $query);
-echo $query."<br>";
-
+//echo $query."<br>";
+$login_check = false;
 // Verifica se la query ha restituito risultati
 if (mysqli_num_rows($result) > 0) {
-    echo "Login riuscito. Benvenuto!"; // L'utente è autenticato con successo
+    $login_msg = "Login riuscito. Benvenuto!"; // L'utente è autenticato con successo
+    $login_check = true;
 } else {
-    echo "Credenziali non valide. Riprova."; // L'utente non è autenticato
+    $login_msg = "Credenziali non valide. Riprova."; // L'utente non è autenticato
 }
 
 // Chiudi la connessione al database
 mysqli_close($conn);
+
+if ($login_check == false) {
+
+    $html = "<p class='error'>$login_msg</p>";
+    $html .= $html_form;
+    $html .= "Non hai un account? <a href='register.php'>Registrati adesso</a>.<br />";
+    $html .= "Hai dimenticato la password? <a href='pwd_reset.php'>Resetta la password</a>.<br />";
+    $html .= "<a href='index.php'>Torna alla Home Page</a>.<br />";
+    
+} else {
+
+
+    $html = "<p class='error'>$errmsg</p>";
+    $html .= "Non hai un account? <a href='register.php'>Registrati adesso</a>.<br />";
+    $html .= "Hai dimenticato la password? <a href='pwd_reset.php'>Resetta la password</a>.<br />";
+    $html .= "<a href='index.php'>Torna alla Home Page</a>.<br />";
+
+}
+
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
     <title>Login</title>
+    <style>
+        .error {color: red}
+    </style> 
 </head>
 <body>
     <h3>Pagina di login</h3>
-    <?php if (isset($error_message)) echo '<p style="color: red;">' . $error_message . '</p>'; ?>
-    <h4>Credenziali:</h4>
-    <h4>username: admin</h4>
-    <h4>password: admin</h4>
-
-    <form method="POST" action="">
-        <label for="username">Nome utente:</label>
-        <input type="text" name="username" required><br><br>
-
-        <label for="password">Password:</label>
-        <input type="password" name="password" required><br><br>
-
-        <input type="submit" value="Accedi">
-    </form>
+    <?=$html?>
 </body>
 </html>
